06626242f0b6ae52489ff0fc9dff4eee0864acb1
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-10-10 00:20:10 +0300

    create project

---
+++ src/ExtEngine/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ExtEngine")>]
+[<assembly: AssemblyProductAttribute("ExtEngine")>]
+[<assembly: AssemblyDescriptionAttribute("small core for extensible project, similary for electron (base of atom-editor), but constructed on .net platform")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
